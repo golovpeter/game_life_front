0244aa7375d4d8c2a798d2eac37f6292320c1cc9
golopetr <petruir@gmail.com>
2022-02-07 13:47:22 +0300

    Change square color

--- src/components/square/square.js
+++ src/components/square/square.js
@@ -4,7 +4,7 @@ export function Square(props) {
     return (
         <button
             className="square"
-            style={{background: props.isSelected ? "hsl(" + Math.random() * 360 + ", 100%, 75%)" : "#fff"}}
+            style={{background: props.isSelected ? "palevioletred" : "#fff"}}
             onMouseEnter={props.onMouseEnter}
             onClick={props.onClick}/>
     );
